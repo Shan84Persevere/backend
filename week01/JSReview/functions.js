92f75dab7780fd2ba74816fd1301e03e151e0552
Shannon Sharma <shayandtrav2024@outlook.com>
2025-01-30 23:41:39 -0600

    redone the test just to see if i could

--- week01/JSReview/functions.js
+++ week01/JSReview/functions.js
@@ -1,40 +1,79 @@
-const returnEmptyString = function () {};
+const returnEmptyString = function () {
+  return "";
+};
 
-const returnZeroNumber = function () {};
+const returnZeroNumber = function () {
+  return 0;
+};
 
-const returnEmptyArray = function () {};
+const returnEmptyArray = function () {
+  return [];
+};
 
-const returnEmptyObject = function () {};
+const returnEmptyObject = function () {
+  return {};
+};
 
-const returnString = function () {};
+const returnString = function (str) {
+  str = 'abc123';
+  return str;
+};
 
-const addition = function () {};
+const addition = function (a,b) {
+  return a + b;
+};
 
-const subtraction = function () {};
+const subtraction = function (a,b) {
+  return a - b;
+};
 
-const multiplication = function () {};
+const multiplication = function (a,b) {
+  return a * b;
+};
 
-const division = function () {};
+const division = function (a,b) {
+  return a / b;
+};
 
-const returnArray = function () {};
+const returnArray = function (arr) {
+  return arr;
+};
 
-const returnFirstIndex = function () {};
+const returnFirstIndex = function (arr) {
+  return arr[0];
+};
 
-const returnSecondIndex = function () {};
+const returnSecondIndex = function (arr) {
+  return arr[1];
+};
 
-const returnArrayLength = function () {};
+const returnArrayLength = function (arr) {
+  return arr.length;
+};
 
-const arraySum = function () {};
+const arraySum = function (arr) {
+  return arr.reduce((a,b) => a + b, 0);
+};
 
-const arraySubtraction = function () {};
+const arraySubtraction = function (arr) {
+  return arr.reduce((a,b) => a - b, 0);
+};
 
-const multiplicationArray = function () {};
+const multiplicationArray = function (arr) {
+  return arr.reduce((a,b) => a * b, 1);
+};
 
-const divisionArray = function () {};
+const divisionArray = function (arr) {
+return Number(arr.reduce((a,b) => parseFloat(a / b, 1).toFixed(3)));
+};
 
-const oddArray = function () {};
+const oddArray = function (arr) {
+  return arr.filter((num) => num % 2 !== 0);
+};
 
-const evenArray = function () {};
+const evenArray = function (arr) {
+return arr.filter((num) => num % 2 === 0);
+};
 
 module.exports = {
 returnEmptyString,
